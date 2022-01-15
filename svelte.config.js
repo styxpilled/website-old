2966cbe85543c2632aac2a5fadaa3c6ddf809689
styxpilled <styxpilled.dev@gmail.com>
2022-01-15 08:22:53 +0100

    refactor: replace highlight.js with shiki

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import path from 'path';
+import sequencial from 'svelte-sequential-preprocessor'
+import { svelteShiki as shiki } from 'svelte-shiki';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+    // If you want to use shiki syntax highlighting, you need to run in the correct order
+    // So it doesn't break when it sees typescript code in your <script> tags
+    // Thats why we use svelte-sequential-preprocessor
+	preprocess:  sequencial([ preprocess(), shiki({ theme: 'Material-Theme-Palenight'}) ]),
 	kit: {
 		adapter: adapter(),
 		target: '#svelte',
