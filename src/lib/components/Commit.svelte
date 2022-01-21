4e9a074dc196ba672cab21e5b789d8800f705e9a
styxpilled <styxpilled.dev@gmail.com>
2022-01-21 14:32:38 +0100

    feat: commit panel

---
+++ src/lib/components/Commit.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    import mdiGithub from '@iconify/icons-mdi/github';
+    import Icon from '@iconify/svelte';
+
+    const url: string = 'https://api.github.com/repos/styxpilled/website/commits/main';
+    let sha: string;
+    function getcommit() {
+        fetch(url)
+        .then(response => response.json())
+        .then(data => sha = data.sha.substring(0, 7))
+		.catch(error => console.log(error));
+    }
+    onMount(() => {
+		getcommit();
+	});
+</script>
+
+<span>
+    <a href="https://github.com/styxpilled/website">
+        <Icon class="icon" icon={mdiGithub} />
+    </a>
+        <code lang="undefined">{sha}:</code> cool femboy facts! do you think god will forgive you for your sins
+</span>
+
+<style lang="scss">
+    a {
+        margin-bottom: -5%;
+        line-height: 0.01rem;
+    }
+    span {
+        font-size: 1.2rem;
+        margin-left: 1rem;
+    }
+</style>
