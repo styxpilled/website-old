e94fd7bcc921f762799e14652742f14839ecac5c
styxpilled <styxpilled.dev@gmail.com>
2022-01-22 09:19:42 +0100

    refactor: move waving hand to a component

---
+++ src/lib/components/Hey.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+	import wavingHand from '@iconify/icons-twemoji/waving-hand';
+</script>
+
+<Icon class="hand" icon={wavingHand} />
+<h1>I'm Styx.</h1>
+
+<style lang="scss">
+	:global(.hand) {
+		animation: wave 15s infinite 5s;
+		font-size: 10rem;
+	}
+
+	@keyframes wave {
+		8% {
+			transform: rotate(60deg);
+		}
+
+		16% {
+			transform: rotate(0deg);
+		}
+	}
+</style>
--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -5,9 +5,7 @@
 	import Credit from './Credit.svelte';
 	import Prophecy from './Prophecy.svelte';
 	import Commit from './Commit.svelte';
-
-	import Icon from '@iconify/svelte';
-	import wavingHand from '@iconify/icons-twemoji/waving-hand';
+	import Hey from './Hey.svelte';
 </script>
 
 <div class="grid" in:fly={{ x: -200, duration: 400, delay: 600 }}>
@@ -15,10 +13,8 @@
 		<Credit />
 	</Panel>
 	<div class="main">
-		<Icon class="hand" icon={wavingHand} />
-		<h1>I'm Styx.</h1>
+		<Hey />
 	</div>
-
 	<div class="panel code">
 	<Panel classname="panel code" direction="right">
 		<Commit />
@@ -27,6 +23,7 @@
 		<code lang="ts">export const variable: string = "gaming"</code>
 	</Panel>
 	</div>
+
 	<Panel classname="panel accounts" direction="left">
 		<code lang="ts">export const variable: string = "gaming"</code>
 	</Panel>
@@ -40,19 +37,4 @@
 
 <style lang="scss">
 	@use 'src/scss/home.scss';
-
-	div :global(.hand) {
-		animation: wave 15s infinite 5s;
-		font-size: 10rem;
-	}
-
-	@keyframes wave {
-		8% {
-			transform: rotate(60deg);
-		}
-
-		16% {
-			transform: rotate(0deg);
-		}
-	}
 </style>
